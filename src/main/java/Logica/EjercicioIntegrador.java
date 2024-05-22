ee6ffa27082c33ff83e3a57a62db295aa5056e0b
rockn <rockn@MuRtPC>
2024-05-22 19:11:25 -0400

    Carga Incial

---
+++ src/main/java/Logica/EjercicioIntegrador.java
@@ -0,0 +1,27 @@
+
+package Logica;
+
+
+public class EjercicioIntegrador {
+
+    public static void main(String[] args) {
+        
+        Squirtle squirtle = new Squirtle ();
+        Charmander charmander = new Charmander ();
+        Bulbasaur bulba = new Bulbasaur();
+        Pikachu pika = new Pikachu ();
+        
+        squirtle.atacarAraniazo();
+        squirtle.atacarHigrobomba();
+        charmander.atacarAraniazo();
+        charmander.atacarLanzallamas();
+        bulba.atacarAraniazo();
+        bulba.atacarDrenaje();
+        pika.atacarAraniazo();
+        pika.atacarPunioTrueno();
+        
+        
+        
+        
+    }
+}
